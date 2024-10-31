687279d2b22f6d4f3e6125c3d598189b1bcdcc9f
reyesjl <reyesjl.nine@hotmail.com>
2024-10-31 18:42:40 -0400

    Story 1 : add id to detail button

--- frontend/src/views/TournamentsView.vue
+++ frontend/src/views/TournamentsView.vue
@@ -12,22 +12,23 @@ onMounted(() => {
 </script>
 
 <template>
-    <main class="container mx-auto p-4">
+    <main>
         <PageHeader 
             title="Tournaments"
             description="Explore a comprehensive list of tournaments submitted by the community. Can’t find a tournament? Add it yourself! Share your experience by leaving a review and help others make informed choices."
         />
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
+
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6 mx-auto w-[92%]">
             <div
                 v-for="tournament in tournamentStore.tournaments"
-                :key="tournament.id"
+                :key="tournament._id"
                 class="bg-gray-600 shadow-lg rounded-lg p-4 flex flex-col"
             >
-                <h2 class="text-xl font-bold mb-2">{{ tournament.name }}</h2>
+                <h2 class="text-white text-xl font-bold mb-2">{{ tournament.name }}</h2>
                 <p class="text-gray-700 mb-4">{{ tournament.description }}</p>
                 <div class="mt-auto">
                     <router-link
-                        :to="`/tournaments/${tournament.id}`"
+                        :to="`/tournaments/${tournament._id}`"
                         class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
                     >
                         View Details
