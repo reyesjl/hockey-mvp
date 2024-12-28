e7480658f03b17aa4e37b627be37608315829976
reyesjl <reyesjl.nine@hotmail.com>
2024-12-28 13:59:05 -0500

    No Story - fix url and .env configs

--- frontend/src/config/apiConfig.ts
+++ frontend/src/config/apiConfig.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
-const API_DEV_URL = import.meta.env.VITE_API_BACKEND_DEV;
-const API_PROD_URL = import.meta.env.VITE_API_BACKEND_PROD;
+
+const API_DEV_URL = import.meta.env.VITE_API_URL_DEV;
+const API_PROD_URL = import.meta.env.VITE_API_URL_PROD;
 const API_BASE_URL = import.meta.env.VITE_NODE_ENV === 'production' ? API_PROD_URL : API_DEV_URL;
 
 export const apiConfig = {
